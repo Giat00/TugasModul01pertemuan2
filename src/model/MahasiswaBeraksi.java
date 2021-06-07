b0dcfe6f89495b8b807aefccdc18c68eddb12594
Giat00 <81604393+Giat00@users.noreply.github.com>
2021-06-07 09:12:39 +0800

    Membuat objek kedua

--- src/model/MahasiswaBeraksi.java
+++ src/model/MahasiswaBeraksi.java
@@ -6,8 +6,21 @@ public class MahasiswaBeraksi {
 	public static void main (String [] args) {
 		Mahasiswa mahasiswa = new Mahasiswa();
 		mahasiswa.npm = "19630944";
-		mahasiswa.nama = "Giat Adi Hatmoojo";
+		mahasiswa.nama = "Giat Adi Hatmojo";
 		mahasiswa.tanggallahir = new Date();
+		
+		System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+		System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+		System.out.println("Tanggal Lahir \t : " + mahasiswa.tanggallahir);
+		
+		Mahasiswa mahasiswa2 = new Mahasiswa();
+		mahasiswa2.npm = "19816494";
+		mahasiswa2.nama = "Hatmojo";
+		mahasiswa2.tanggallahir = new Date();
+		
+		System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+		System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+		System.out.println("Tanggal Lahir \t : " + mahasiswa2.tanggallahir);
 	}
 		
 }
